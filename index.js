76a85d1b365aede1a70214a34f62d81eb0843e29
BintangSaputra <bintangcbs26@gmail.com>
2019-04-15 11:07:34 +0700

    add files

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(parseInt("200px"));
+console.log(parseInt("20.5"));
+
+console.log(parseFloat("12.123"));
+console.log(parseFloat("1223.23.23.23"));
+
+alert(Math.random());
+alert(Math.random(2));
